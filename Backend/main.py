4dc66652ae091dce818b7b19559c1d8f4f043bac
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2024-08-10 22:32:07 +0530

    main.py modified

--- Backend/main.py
+++ Backend/main.py
@@ -1,21 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-import pandas as pd
+from crop_recommendation.recommendation import predict_crop # type: ignore
+from crop_rotation.rotation import rotate_crop # type: ignore
 
 app = FastAPI()
 
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins, adjust as needed
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods, adjust as needed
-    allow_headers=["*"],  # Allow all headers, adjust as needed
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
-# Load your dataset
-df = pd.read_csv('Crop_Recommendation.csv')
 
 class CropData(BaseModel):
     N: float
@@ -26,28 +24,10 @@ class CropData(BaseModel):
     ph: float
     rainfall: float
 
-def predict_crop(data: CropData):
-    best_match = None
-    min_distance = float('inf')
-
-    for index, row in df.iterrows():
-        distance = (
-            abs(row['N'] - data.N) +
-            abs(row['P'] - data.P) +
-            abs(row['K'] - data.K) +
-            abs(row['temperature'] - data.temperature) +
-            abs(row['humidity'] - data.humidity) +
-            abs(row['ph'] - data.ph) +
-            abs(row['rainfall'] - data.rainfall)
-        )
-
-        if distance < min_distance:
-            min_distance = distance
-            best_match = row['label']
-
-    return best_match
-
 @app.post("/predict/")
 def predict(data: CropData):
-    crop = predict_crop(data)
-    return {"crop": crop}
+    crop_data = data.model_dump()  # Use model_dump() instead of dict()
+    crop = predict_crop(crop_data)
+    rotated_crop = rotate_crop(crop)
+    
+    return {"crop": crop, "rotatedCrop": rotated_crop}
