5a1ca35a95cb03a3419214a9d54a50b0b656ef4e
ashishrathod9 <ashishrathod53839@gmail.com>
2024-08-11 08:59:28 +0530

    Navbar.jsx updated

--- frontend_form/src/components/Navbar.jsx
+++ frontend_form/src/components/Navbar.jsx
@@ -1,17 +1,25 @@
-// src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { scroller } from 'react-scroll';
 
 function Navbar() {
+  const handleGetStarted = () => {
+    scroller.scrollTo('suggestedCrop', {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+    });
+  };
+
   return (
-    <nav className="bg-green-800 p-4 shadow-lg">
+    <nav className="bg-green-600 p-4 shadow-md sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white text-2xl font-bold">Crop Detection</Link>
-        <div className="space-x-4">
-          <Link to="/" className="text-white text-lg hover:text-green-200">Home</Link>
-          <Link to="/calculate" className="text-white text-lg hover:text-green-200">Suggest-crop</Link>
-          <Link to="/FarmerGuide" className="text-white text-lg hover:text-green-200">Guide</Link>
-        </div>
+        <Link to="/" className="text-white text-2xl font-bold">CropDetector</Link>
+        <ul className="flex space-x-8">
+          <li><Link to="/" className="text-2xl text-white transition duration-300 transform hover:scale-110 hover:underline">Home</Link></li>
+          <li><Link to="/calculate" className="text-2xl text-white transition duration-300 transform hover:scale-110 hover:underline">Calculate</Link></li>
+          <li><Link to="/FarmerGuide" className="text-2xl text-white transition duration-300 transform hover:scale-110 hover:underline">Guide</Link></li>
+        </ul>
       </div>
     </nav>
   );
